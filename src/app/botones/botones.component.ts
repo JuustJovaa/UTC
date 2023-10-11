6da0af4c0b32c72be7c42ab39d6cd67c3760bb93
Jovani Garcia <justjova@MacBook-Air-de-Jovani.local>
2023-10-11 17:25:53 -0600

    components about Angular course

---
+++ src/app/botones/botones.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botones',
+  templateUrl: './botones.component.html',
+  styleUrls: ['./botones.component.css']
+})
+export class BotonesComponent {
+
+/*
+curso botones dinámicos
+textColor: string = '';
+
+  buttonDisabled: boolean = false;
+
+  switchButton() {
+    this.buttonDisabled = !this.buttonDisabled;
+  } */
+
+  srcImage: string = "https://images.pexels.com/photos/8955380/pexels-photo-8955380.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load";
+}
